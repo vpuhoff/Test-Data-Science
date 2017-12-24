ad2eb0ab214682fd13c9beaed68579a6b62b6909
neuroware <vpuhoff@live.ru>
2017-12-25 00:43:44 +0300

    9

---
+++ 9example/9example/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _9example
+{
+    class Program
+    {
+        static int GlobalVar = 1;
+        static void Main(string[] args)
+        {
+            Func1();
+            Func2();
+            Console.ReadLine();
+        }
+        static void Func1()
+        {
+            GlobalVar++;
+            Console.WriteLine(GlobalVar);
+        }
+        static void Func2()
+        {
+            GlobalVar= GlobalVar*2;
+            Console.WriteLine(GlobalVar);
+        }
+    }
+}
