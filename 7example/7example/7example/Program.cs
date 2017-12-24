19dc81c030477314ba7b8013812ff6d1a2827f70
neuroware <vpuhoff@live.ru>
2017-12-25 00:25:17 +0300

    5

---
+++ 7example/7example/7example/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7example
+{
+    class Человек
+    {
+        public int Вес;
+        public int Рост;
+        public string Пол;
+        public int Возраст;
+        public virtual void СделатьЧтото()
+        {
+            Console.WriteLine("Мне лень!");
+        }
+    }
+    class Мужчина : Человек
+    {
+        new string Пол = "МУЖ";
+        public override void СделатьЧтото()
+        {
+            Console.WriteLine(Пол+":Забил гвоздь!"); 
+        }
+    }
+    class Женщина : Человек
+    {
+        new string Пол = "ЖЕН";
+        public override void СделатьЧтото()
+        {
+            Console.WriteLine(Пол+":Полила цветы!");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Вася = new Мужчина();
+            var Маша = new Женщина();
+            var Чел  = new Человек();
+            Вася.СделатьЧтото();
+            Маша.СделатьЧтото();
+            Чел.СделатьЧтото();
+            Console.ReadLine();
+            //МУЖ: Забил гвоздь!
+            //ЖЕН:Полила цветы!
+            //Мне лень!
+        }
+    }
+}
