7ab3f8dbc68b59b77e96d2cfa19370b10ae9ad19
vpuhoff <vpuhoff@live.ru>
2018-01-09 00:05:28 +0300

    24 задание

--- 24Example/Program.cs
+++ 24Example/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,19 @@ namespace _24Example
     {
         static void Main(string[] args)
         {
+            var folder = System.IO.Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            Console.WriteLine(folder);
+            var files = Directory.EnumerateFiles(folder);
+            foreach (var file in files)
+            {
+                var FI = new FileInfo(file);
+                var delay = DateTime.Now - FI.CreationTime  ;
+                if ( delay.TotalDays >3)
+                {
+                    Console.WriteLine("{0} Created {1} days ago\t at {2}", FI.Name, delay.TotalDays, FI.CreationTime);
+                }
+            }
+            Console.ReadLine();
         }
     }
 }
