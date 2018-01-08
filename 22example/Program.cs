f5db913fa00ef37fa10d8ef14fc15b352be176f9
vpuhoff <vpuhoff@live.ru>
2018-01-08 23:47:57 +0300

    23 задача

---
+++ 22example/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _22example
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение N:");
+            var n = int.Parse(Console.ReadLine());
+            var maxb = Math.Floor(Math.Sqrt(n));
+            bool finded = false;
+            if (maxb >0 )
+            {
+                for (double  b = 1.0; b <= maxb ; b=b+1)
+                {
+                    double a = Math.Sqrt(n - b * b);
+                    if (a%1==0)//Число является целым
+                    {
+                        var s = a * a + b * b;
+                        if (s==n) //Проверка
+                        {
+                            Console.WriteLine("a={0}\tb={1}\ta^2+b^2={2}+{3}={4}",a,b,a*a,b*b,s);
+                            finded = true;
+                        }
+                        else
+                        {
+                            Console.WriteLine("Ошибка в алгоритме!");
+                        }
+                    }
+                }
+                if (!finded )
+                {
+                    Console.WriteLine("Решения нет.");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Решения нет.");
+            }
+        }
+
+        
+    }
+}
---
+++ 23Example/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Code
+{
+    class Program
+    {
+        public static bool isPalRecu(string s)
+        {
+            if (s.Length == 0 || s.Length == 1)//больше нет символов либо остался лишь один символ в центре строки
+                return true;
+            if (s[0] == s[s.Length - 1])//сравниваем первый символ с последним
+                return isPalRecu(s.Substring(1, s.Length - 2));//отправляем на проверку содержимое строки между первым и последним символом                
+            return false;
+        }
+        static void Main(string[] args)
+        {
+            var s = Console.ReadLine();
+            if (isPalRecu(s))
+            {
+                Console.WriteLine("Это полиндром!");
+            }
+            else
+            {
+                Console.WriteLine("Это не полиндром!");
+            }
+        }
+    }
+}
