977fc91eb8c8d43321f1cede449da355ee3be72b
vpuhoff <vpuhoff@live.ru>
2018-01-08 19:20:50 +0300

    14 задание

---
+++ 14example/14example/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _14example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n:");
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите S:");
+            int s = int.Parse(Console.ReadLine());
+            int[] A = new int[n];
+            for (int x = 0; x < n; x++)
+            {
+                A[x] = x+1;
+            }
+            int i,j,ai,aj=0; //инициализация массива А и переменных i,j
+      
+            if (n>=s) // Чисел достаточно, чтобы сразу найти одно решение
+            {
+                i = 0;
+                j = s -2;
+                ai = A[i];
+                aj = A[j];
+                if (i==j)
+                {
+                    Console.WriteLine("Решения не существует");
+                }
+                Console.WriteLine("i={0}\tj={1}\t{2}+{3}={4}",i,j,ai,aj,s);
+                Assert(ai + aj, s);//Сравним с эталоном и либо сообщим что корректно, либо сообщим об ошибке
+            }
+            else
+            {
+                if (s-n>=n)
+                {
+                    //Это значит, что второе слагаемое либо больше либо равно n, что по условию задачи не допустимо
+                    Console.WriteLine("Решение не существует");
+                }
+                else
+                {
+                    j = n-1;
+                    i = s - n-1;
+                    ai = A[i];
+                    aj = A[j];
+                    Console.WriteLine("i={0}\tj={1}\t{2}+{3}={4}", i, j, ai, aj, s);
+                    Assert(ai + aj, s);//Сравним с эталоном и либо сообщим что корректно, либо сообщим об ошибке
+                }
+            }
+            Console.ReadLine();
+        }
+
+        private static void Assert(int v, int s)
+        {
+            if (v==s)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Верно: {0}={1}", v, s);
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Ошмбка: {0}!={1}", v, s);
+            }
+        }
+    }
+}
---
+++ 14example/14example/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("14example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("14example")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("bf99b3e5-b642-438d-bbff-90aec198487f")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
